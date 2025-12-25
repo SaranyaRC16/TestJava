6078873068524d7b09b04b311327c64d7b17f8d6
Saranya RC <rcsaranya16@gmail.com>
2025-12-25 14:36:36 +0530

    Update Test.java

--- src/main/java/UserProfile/Test.java
+++ src/main/java/UserProfile/Test.java
@@ -8,6 +8,7 @@ public class Test {
 
 	public void verifyUserProfile() {
 		System.out.println("User Profile Testing");
+		System.out.println("Check for Link");
 	}
 
 	public void takeUserProfile() {
